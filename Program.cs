10d19a7be26ca857d8ebed0a8f0b09a53c630b02
Sukharevich Vladislav <vladoslav.sukharevich3@gmail.com>
2021-03-11 10:54:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите K: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            double S = 0;
+            for (int n = 0; n <= k; n++)
+            {
+                S += Math.Pow(-1, n) * (Math.Pow(x, 2 * n + 1) / ((2 * n - 1) * Fact(2 * n + 1)));
+            }
+            Console.WriteLine($"Сумма ряда: {S}");
+        }
+        static int Fact(int x)
+        {
+            int result = 1;
+            for (int i = 1; i <= x; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
